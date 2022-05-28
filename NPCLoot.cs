e83d29dfb1ff4d468dde1563ed4f2e1f033df798
wdfeer <wdf-is-git@tutanota.de>
2022-05-28 21:57:58 +0200

    Add the Magnetize item

---
+++ NPCLoot.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WarframeMod
+{
+    internal class NPCLoot : GlobalNPC
+    {
+        public IItemDropRule GetItemDropRuleForType(int type)
+        {
+            switch (type)
+            {
+                case NPCID.SkeletronPrime:
+                    return ItemDropRule.Common(ModContent.ItemType<Items.Magnetize>(), 10);
+                default:
+                    return null;
+            }
+        }
+        public override void ModifyNPCLoot(NPC npc, Terraria.ModLoader.NPCLoot npcLoot)
+        {
+            var dropRule = GetItemDropRuleForType(npc.type);
+            if (dropRule != null)
+                npcLoot.Add(dropRule);
+        }
+    }
+}
---
+++ Projectiles/MagnetizeProjectile.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Xna.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace WarframeMod.Projectiles
+{
+    internal class MagnetizeProjectile : ModProjectile
+    {
+        public override string Texture => "Terraria/Images/Projectile_" + ProjectileID.DiamondBolt;
+        public NPC target;
+        public override void SetDefaults()
+        {
+            Projectile.hide = true;
+            Projectile.width = 240;
+            Projectile.timeLeft = 600;
+            Projectile.tileCollide = false;
+            Projectile.penetrate = -1;
+        }
+        public override void AI()
+        {
+            if (target == null || !target.active)
+            {
+                Projectile.timeLeft = 0;
+                return;
+            }
+            Projectile.Center = target.Center;
+
+            Projectile[] projectiles = GetIntersectingProjectiles();
+            foreach (var proj in projectiles)
+            {
+                proj.velocity += Vector2.Normalize(Projectile.Center - proj.Center);
+            }
+
+            SpawnDusts();
+        }
+        public override bool ShouldUpdatePosition() => false;
+        Projectile[] GetIntersectingProjectiles()
+        {
+            return Main.projectile.Where(x => x.active && x.friendly && !Main.projPet[x.type] && x.Center.Distance(Projectile.Center) < Projectile.width).ToArray();
+        }
+        void SpawnDusts()
+        {
+            for (int i = 0; i < Projectile.width / 8; i++)
+            {
+                Vector2 position = Projectile.Center + Main.rand.NextVector2CircularEdge(Projectile.width, Projectile.width);
+                Dust d = Dust.NewDustDirect(position, 1, 1, DustID.MagnetSphere);
+                d.noGravity = true;
+            }
+        }
+    }
+}
