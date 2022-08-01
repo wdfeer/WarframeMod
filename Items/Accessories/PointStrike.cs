85646df99bbc09553466460e7e63cc01ad9b162e
wdfeer <wdf-is-git@tutanota.de>
2022-08-01 10:07:31 +0200

    Add PointStrike

---
+++ Items/Accessories/PointStrike.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WarframeMod.Players;
+
+namespace WarframeMod.Items.Accessories
+{
+    public class PointStrike : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("+66% Crit Chance relative to the weapon's base crit chance");
+        }
+        public override void SetDefaults()
+        {
+            Item.accessory = true;
+            Item.width = 44;
+            Item.height = 64;
+            Item.rare = 2;
+            Item.value = Terraria.Item.sellPrice(gold: 1);
+        }
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (player.HeldItem.damage > 0)
+                player.GetCritChance(DamageClass.Generic) += (player.HeldItem.crit + 4) * 0.66f;
+        }
+    }
+}
