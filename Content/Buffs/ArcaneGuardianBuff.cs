9ed4e29ea295a53f57145f2fbddb92a65b5180c8
wdfeer <wdf-is-git@tutanota.de>
2022-08-19 09:18:29 +0200

    Add ArcaneGuardian

---
+++ Content/Buffs/ArcaneGuardianBuff.cs
@@ -0,0 +1,25 @@
+using Terraria;
+using Terraria.ModLoader;
+using WarframeMod.Content.Items.Accessories;
+
+namespace WarframeMod.Content.Buffs;
+public class ArcaneGuardianBuff : ModBuff
+{
+    public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("Arcane Guardian");
+        Description.SetDefault("+1 Defense");
+        Main.pvpBuff[Type] = true;
+        Main.buffNoSave[Type] = true;
+    }
+    int defense = 1;
+    public override void ModifyBuffTip(ref string tip, ref int rare)
+    {
+        tip = $"+{defense} Defense";
+    }
+    public override void Update(Player player, ref int buffIndex)
+    {
+        defense = player.GetModPlayer<GuardianPlayer>().currentDefense;
+        player.statDefense += defense;
+    }
+}
---
+++ Content/Items/Accessories/ArcaneGuardian.cs
@@ -0,0 +1,57 @@
+using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WarframeMod.Content.Buffs;
+
+namespace WarframeMod.Content.Items.Accessories;
+
+public class ArcaneGuardian : ModItem
+{
+    public const int DAMAGE_TO_DEFENSE_RATIO = 5;
+    public const int BUFF_DURATION = 300;
+    public override void SetStaticDefaults()
+    {
+        Tooltip.SetDefault($"On hit: for every {DAMAGE_TO_DEFENSE_RATIO} points of damage taken receive +1 Defense for {BUFF_DURATION / 60} seconds");
+    }
+
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        Item.accessory = true;
+        Item.rare = -12;
+        Item.width = 32;
+        Item.height = 32;
+        Item.value = Item.buyPrice(gold: 4);
+    }
+    public override void UpdateInventory(Player player)
+    {
+        Item.rare = -12;
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<GuardianPlayer>().enabled = true;
+    }
+}
+class GuardianPlayer : ModPlayer
+{
+    public bool enabled;
+    public override void ResetEffects()
+        => enabled = false;
+    public int currentDefense = 0;
+    void ApplyBuff(int damage)
+    {
+        currentDefense = (int)MathF.Ceiling((float)damage / ArcaneGuardian.DAMAGE_TO_DEFENSE_RATIO);
+        Player.AddBuff(ModContent.BuffType<ArcaneGuardianBuff>(), ArcaneGuardian.BUFF_DURATION);
+    }
+    public override void OnHitByNPC(NPC npc, int damage, bool crit)
+    {
+        ApplyBuff(damage);
+    }
+    public override void OnHitByProjectile(Projectile proj, int damage, bool crit)
+    {
+        ApplyBuff(damage);
+    }
+}
