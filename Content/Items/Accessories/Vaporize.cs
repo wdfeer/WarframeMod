af99cd84e368c203d82f2d447dc8fca221962856
wdfeer <wdf-is-git@tutanota.de>
2022-08-21 15:15:26 +0200

    Fix Vaporize affecting inactive minions and enemies

--- Content/Items/Accessories/Vaporize.cs
+++ Content/Items/Accessories/Vaporize.cs
@@ -4,13 +4,13 @@ namespace WarframeMod.Content.Items.Accessories;
 
 public class Vaporize : ModItem
 {
-    public const int DAMAGE = 90;
+    public const int DAMAGE = 100;
     public const float KNOCKBACK = 4.5f;
     public const int COOLDOWN = 480;
     public const int MAX_ATTACK_DISTANCE = 750;
     public override void SetStaticDefaults()
     {
-        Tooltip.SetDefault($"All minions will deal {DAMAGE} damage to a nearby enemy every {COOLDOWN / 60} seconds");
+        Tooltip.SetDefault($"All minions will deal {DAMAGE} damage to nearby enemies every {COOLDOWN / 60} seconds");
     }
     public override void SetDefaults()
     {
@@ -20,7 +20,7 @@ public class Vaporize : ModItem
         Item.rare = 5;
         Item.value = Item.sellPrice(gold: 2);
     }
-    int timer = 0;
+    int timer = COOLDOWN;
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
         timer++;
@@ -36,11 +36,11 @@ public class Vaporize : ModItem
     }
     NPC[] FindTargets(Player player)
     {
-        return Main.npc.Where(npc => !npc.friendly && npc.CanBeChasedBy() && npc.Center.Distance(player.Center) < MAX_ATTACK_DISTANCE).ToArray();
+        return Main.npc.Where(npc => npc.active && !npc.friendly && npc.CanBeChasedBy() && npc.Center.Distance(player.Center) < MAX_ATTACK_DISTANCE).ToArray();
     }
     void Fire(Player player, IList<NPC> targets)
     {
-        IEnumerable<Projectile> minions = Main.projectile.Where(proj => proj.friendly && proj.owner == player.whoAmI && proj.minionSlots > 0);
+        IEnumerable<Projectile> minions = Main.projectile.Where(proj => proj.active && proj.friendly && proj.owner == player.whoAmI && proj.minionSlots > 0);
         foreach (Projectile projectile in minions)
         {
             NPC target = targets[Main.rand.Next(0,targets.Count)];
@@ -49,6 +49,7 @@ public class Vaporize : ModItem
             Vector2 velocity = Vector2.Normalize(to - from) * 16f;
             Projectile shot = Projectile.NewProjectileDirect(Item.GetSource_FromThis(), from, velocity, ProjectileID.DiamondBolt, DAMAGE, KNOCKBACK, player.whoAmI);
             shot.tileCollide = false;
+            shot.penetrate = -1;
             shot.usesLocalNPCImmunity = true;
             shot.localNPCHitCooldown = -1;
             shot.extraUpdates = 6;
