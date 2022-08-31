d79853c122ebeae4878e946d677ac43a5a7af42b
wdfeer <wdf-is-git@tutanota.de>
2022-08-31 09:40:52 +0200

    Add PiercingCaliber

--- Content/Items/Accessories/PiercingCaliber.cs
+++ Content/Items/Accessories/PiercingCaliber.cs
@@ -3,10 +3,10 @@ using WarframeMod.Common.Players;
 
 namespace WarframeMod.Content.Items.Accessories;
 
-public class PiercingHit : ModItem
+public class PiercingCaliber : ModItem
 {
-    public const float CHANCE = 0.12f;
-    public const int DURATION = 360;
+    public const float CHANCE = 0.2f;
+    public const int DURATION = 720;
     public override void SetStaticDefaults()
     {
         Tooltip.SetDefault($"+{(int)(CHANCE * 100)}% Chance to inflict the Weakened debuff for {DURATION / 60} seconds, decreasing enemy contact damage with each proc");
@@ -16,8 +16,16 @@ public class PiercingHit : ModItem
         Item.accessory = true;
         Item.width = 44;
         Item.height = 64;
-        Item.rare = 2;
-        Item.value = Item.sellPrice(silver: 20);
+        Item.rare = 6;
+        Item.value = Item.sellPrice(gold: 2);
+    }
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ModContent.ItemType<PiercingHit>());
+        recipe.AddIngredient(ItemID.HallowedBar, 4);
+        recipe.AddTile(TileID.MythrilAnvil);
+        recipe.Register();
     }
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
