08af931d9cc5b7d0acfb0901a384ed16bce07d2f
wdfeer <wdf-is-git@tutanota.de>
2022-09-28 09:04:15 +0200

    Add ArmoredAgility

---
+++ Common/Players/DamageResistancePlayer.cs
@@ -0,0 +1,22 @@
+﻿using Terraria.DataStructures;
+
+namespace WarframeMod.Common.Players;
+
+internal class DamageResistancePlayer : ModPlayer
+{
+    public List<float> resists = new List<float>();
+    public float DamageMultiplier => resists.Any() ? resists.Select(x => 1 - x).Aggregate((x,y) => x * y) : 1f;
+    public override void ResetEffects()
+    {
+        resists = new List<float>();
+    }
+    public void AddDR(float resist)
+    {
+        resists.Add(resist);
+    }
+    public override bool PreHurt(bool pvp, bool quiet, ref int damage, ref int hitDirection, ref bool crit, ref bool customDamage, ref bool playSound, ref bool genGore, ref PlayerDeathReason damageSource, ref int cooldownCounter)
+    {
+        damage = (int)(damage * DamageMultiplier);
+        return base.PreHurt(pvp, quiet, ref damage, ref hitDirection, ref crit, ref customDamage, ref playSound, ref genGore, ref damageSource, ref cooldownCounter);
+    }
+}
---
+++ Content/Items/Accessories/ArmoredAgility.cs
@@ -0,0 +1,32 @@
+﻿using WarframeMod.Common.Players;
+
+namespace WarframeMod.Content.Items.Accessories;
+
+public class ArmoredAgility : ModItem
+{
+    public override void SetStaticDefaults()
+    {
+        Tooltip.SetDefault("+6% damage resistance\n+10% movement speed");
+    }
+    public override void SetDefaults()
+    {
+        Item.accessory = true;
+        Item.width = 44;
+        Item.height = 64;
+        Item.rare = 1;
+        Item.value = Item.sellPrice(silver: 33);
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<DamageResistancePlayer>().AddDR(0.06f);
+        player.moveSpeed += 0.1f;
+    }
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.Shackle);
+        recipe.AddIngredient(ItemID.Aglet);
+        recipe.AddTile(TileID.TinkerersWorkbench);
+        recipe.Register();
+    }
+}
