4fdc238e0cff783b79d9adb18c918dc14d76a2fa
wdfeer <wdf-is-git@tutanota.de>
2022-09-29 17:02:11 +0200

    Add RaktaCernos

--- Content/Items/Weapons/RaktaCernos.cs
+++ Content/Items/Weapons/RaktaCernos.cs
@@ -2,27 +2,28 @@ using Terraria.DataStructures;
 
 namespace WarframeMod.Content.Items.Weapons;
 
-internal class Cernos : ModItem
+internal class RaktaCernos : ModItem
 {
     public override void SetStaticDefaults()
     {
-        Tooltip.SetDefault("Shoots high velocity arrows with extra penetration");
+        Tooltip.SetDefault("Shoots high velocity arrows");
     }
     public override void SetDefaults()
     {
-        Item.damage = 16;
-        Item.crit = 32;
-        Item.knockBack = 7;
+        Item.damage = 48;
+        Item.crit = 31;
+        Item.knockBack = 4;
         Item.DamageType = DamageClass.Ranged;
         Item.noMelee = true;
-        Item.width = 36;
-        Item.height = 54;
+        Item.width = 41;
+        Item.height = 64;
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.UseSound = SoundID.Item5;
-        Item.useTime = 30;
-        Item.useAnimation = 30;
-        Item.rare = 2;
-        Item.value = 6000;
+        Item.useTime = 15;
+        Item.useAnimation = 15;
+        Item.autoReuse = true;
+        Item.rare = 4;
+        Item.value = Item.sellPrice(gold: 2);
         Item.shoot = ProjectileID.WoodenArrowFriendly;
         Item.shootSpeed = 16f;
         Item.useAmmo = AmmoID.Arrow;
@@ -33,22 +34,16 @@ internal class Cernos : ModItem
     }
     public override void AddRecipes()
     {
-        int[] bowTypes = { ItemID.SilverBow, ItemID.TungstenBow };
-        for (int i = 0; i < bowTypes.Length; i++)
-        {
-            Recipe recipe = CreateRecipe();
-            recipe.AddIngredient(bowTypes[i]);
-            recipe.AddIngredient(ItemID.JungleSpores, 5);
-            recipe.AddTile(TileID.Anvils);
-            recipe.Register();
-        }
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.SoulofNight, 17);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
     }
     public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
     {
         WeaponCommon.ModifyProjectileSpawnPosition(ref position, velocity, Item.width);
         int projectileID = Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
         Projectile projectile = Main.projectile[projectileID];
-        if (projectile.penetrate != -1) projectile.penetrate++;
         projectile.usesLocalNPCImmunity = true;
         projectile.localNPCHitCooldown = -1;
         (Mod as WarframeMod).SetProjectileExtraUpdatesNetSafe(projectileID, projectile.extraUpdates + 1);
