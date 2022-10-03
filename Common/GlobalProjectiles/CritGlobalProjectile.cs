e760cff7b5d9c7775651e254c139f538a5bf8c2f
wdfeer <wdf-is-git@tutanota.de>
2022-10-03 13:20:43 +0200

    Fix minions not resetting critchance upon unequipping crit-boosting equipment

--- Common/GlobalProjectiles/CritGlobalProjectile.cs
+++ Common/GlobalProjectiles/CritGlobalProjectile.cs
@@ -4,4 +4,18 @@ internal class CritGlobalProjectile : GlobalProjectile
 {
     public override bool InstancePerEntity => true;
     public float CritMultiplier { get; set; } = 1f;
+    public override void PostAI(Projectile projectile)
+    {
+        if (Main.projPet[projectile.type] && projectile.damage > 0)
+        {
+            Player owner = Main.player[projectile.owner];
+            Item spawnedFrom = owner.inventory.First(i => i.shoot == projectile.type);
+            int crit = 4;
+            if (spawnedFrom != null)
+                crit = owner.GetWeaponCrit(spawnedFrom);
+            else
+                crit = (int)owner.GetTotalCritChance(projectile.DamageType);
+            projectile.CritChance = crit;
+        }
+    }
 }
