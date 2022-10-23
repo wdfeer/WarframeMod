3f16dd677a2f08e565a60103699fc34179e83650
wdfeer <wdf-is-git@tutanota.de>
2022-10-23 16:35:59 +0200

    Add CryoRounds

---
+++ Content/Buffs/ColdDebuff.cs
@@ -0,0 +1,29 @@
+namespace WarframeMod.Content.Buffs;
+
+public class ColdDebuff : ModBuff
+{
+    public override string Texture => "Terraria/Images/Buff_" + BuffID.Slow;
+    public override void SetStaticDefaults()
+    {
+        DisplayName.SetDefault("Slowed");
+        Main.buffNoSave[Type] = true;
+        Main.debuff[Type] = true;
+    }
+}
+internal class ColdDebuffGlobalNPC : GlobalNPC
+{
+    public override void AI(NPC npc)
+    {
+        if (!npc.HasBuff<ColdDebuff>())
+            return;
+        if (Main.rand.NextBool(WarframeMod.IsBossAlive() ? 4 : 1))
+        {
+            npc.velocity.X *= 0.95f;
+            npc.velocity.Y *= 0.975f;
+        }
+
+        Dust d = Dust.NewDustDirect(npc.position, npc.width, npc.height, DustID.Frost);
+        d.velocity *= 0.9f;
+        d.noGravity = true;
+    }
+}
---
+++ Content/Items/Accessories/CryoRounds.cs
@@ -0,0 +1,33 @@
+﻿using WarframeMod.Common;
+using WarframeMod.Common.Players;
+using WarframeMod.Content.Buffs;
+
+namespace WarframeMod.Content.Items.Accessories;
+
+public class CryoRounds : ModItem
+{
+    public const int CHANCE_PERCENT = 15;
+    public const int DEBUFF_DURATION = 360;
+    public override void SetStaticDefaults()
+    {
+        Tooltip.SetDefault($"+{CHANCE_PERCENT}% chance to slow enemies down for {DEBUFF_DURATION / 60} seconds");
+    }
+    public override void SetDefaults()
+    {
+        Item.accessory = true;
+        Item.width = 44;
+        Item.height = 64;
+        Item.rare = 1;
+        Item.value = Item.sellPrice(silver: 25);
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        BuffPlayer buffman = player.GetModPlayer<BuffPlayer>();
+        buffman.buffsOnHitNPC.Add(new BuffChance(ModContent.BuffType<ColdDebuff>(), DEBUFF_DURATION, CHANCE_PERCENT));
+    }
+    public override void AddRecipes()
+        => CreateRecipe().AddRecipeGroup(RecipeGroupID.IronBar, 3)
+                         .AddIngredient(ItemID.IceBlock, 50)
+                         .AddTile(TileID.Anvils)
+                         .Register();
+}
