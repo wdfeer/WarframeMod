f352a7a53a776eb1b11a9af81e34093044ee2acb
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-10-27 13:51:23 +0200

    Add KuvaSeer weapon

---
+++ Content/Items/Weapons/KuvaSeer.cs
@@ -0,0 +1,51 @@
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.Localization;
+using WarframeMod.Common.GlobalProjectiles;
+using WarframeMod.Content.Projectiles;
+
+
+namespace WarframeMod.Content.Items.Weapons;
+public class KuvaSeer : ModItem
+{
+    public const int EXPLOSION_DAMAGE_PERCENT = 50;
+    public const int ICHOR_CHANCE = 33;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(EXPLOSION_DAMAGE_PERCENT, ICHOR_CHANCE);
+    public override void SetDefaults()
+    {
+        Item.damage = 69;
+        Item.crit = 17;
+        Item.DamageType = DamageClass.Ranged;
+        Item.width = 32;
+        Item.height = 19;
+        Item.useTime = 24;
+        Item.useAnimation = 24;
+        Item.useStyle = ItemUseStyleID.Shoot;
+        Item.noMelee = true;
+        Item.knockBack = 4;
+        Item.value = Item.sellPrice(gold: 1);
+        Item.rare = ItemRarityID.Pink;
+        Item.UseSound = SoundID.Item41;
+        Item.autoReuse = false;
+        Item.shoot = 10;
+        Item.shootSpeed = 16f;
+        Item.useAmmo = AmmoID.Bullet;
+    }
+    public override Vector2? HoldoutOffset()
+    {
+        return new Vector2(-0.5f, 0);
+    }
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+    {
+        Projectile proj = this.ShootWith(player, source, position, velocity, ModContent.ProjectileType<KuvaSeerProjectile>(), damage, knockback, 0.01f, Item.width);
+        proj.GetGlobalProjectile<BuffGlobalProjectile>().AddBuff(new Common.BuffChance(BuffID.Ichor, 300, ICHOR_CHANCE));
+        return false;
+    }
+    public override void AddRecipes()
+    {
+        var recipe = CreateRecipe();
+        recipe.AddIngredient<Seer>();
+        recipe.AddIngredient<Kuva>(2);
+        recipe.Register();
+    }
+}
---
+++ Content/Projectiles/KuvaSeerProjectile.cs
@@ -0,0 +1,37 @@
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.GameContent;
+using Terraria.ID;
+using WarframeMod.Content.Items.Weapons;
+
+namespace WarframeMod.Content.Projectiles;
+
+internal class KuvaSeerProjectile : ExplosiveProjectile
+{
+    const int BASE_BULLET_TYPE = ProjectileID.ExplosiveBullet;
+    public override string Texture => "Terraria/Images/Projectile_" + BASE_BULLET_TYPE;
+    public override int ExplosionWidth => 120;
+    public override void SetDefaults()
+    {
+        Projectile.CloneDefaults(BASE_BULLET_TYPE);
+        AIType = BASE_BULLET_TYPE;
+
+        base.SetDefaults();
+
+        Projectile.DamageType = DamageClass.Ranged;
+    }
+    public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+    {
+        if (exploding)
+        {
+            modifiers.SourceDamage *= KuvaSeer.EXPLOSION_DAMAGE_PERCENT / 100f;
+        }
+    }
+    public override void ExplosionDusts()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            int dustIndex = Dust.NewDust(new Vector2(Projectile.position.X, Projectile.position.Y), Projectile.width, Projectile.height, DustID.Smoke, 0f, 0f, 100, default(Color), 1.5f);
+            Main.dust[dustIndex].velocity *= 1f;
+        }
+    }
+}
