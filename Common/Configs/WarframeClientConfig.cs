2d14751e5dd70eaf44a451260c0ea4b0b02e1693
wdfeer <49950781+wdfeer@users.noreply.github.com>
2023-10-30 20:43:59 +0100

    Make client config have only 1 header

--- Common/Configs/WarframeClientConfig.cs
+++ Common/Configs/WarframeClientConfig.cs
@@ -7,16 +7,13 @@ public class WarframeClientConfig : ModConfig
 {
     public override ConfigScope Mode => ConfigScope.ClientSide;
 
-    [Header("Config.noCritHitColor")]
+    [Header("Config.hitcolors")]
     [DefaultValue(typeof(Color), "245, 245, 245, 255")]
     public Color noCritHitColor;
-    [Header("Config.tier1CritColor")]
     [DefaultValue(typeof(Color), "255, 255, 0, 255")]
     public Color tier1CritColor;
-    [Header("Config.tier2CritColor")]
     [DefaultValue(typeof(Color), "255, 166, 0, 255")]
     public Color tier2CritColor;
-    [Header("Config.maxCritColor")]
     [DefaultValue(typeof(Color), "255, 0, 0, 255")]
     public Color maxCritColor;
 }
