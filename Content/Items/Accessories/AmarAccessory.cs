4a386544881883b65ca67848f7d7e16894a773cf
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-19 17:24:16 +0100

    Create abstract AmarAccessory

---
+++ Content/Items/Accessories/AmarAccessory.cs
@@ -0,0 +1,74 @@
+﻿namespace WarframeMod.Content.Items.Accessories;
+
+public abstract class AmarAccessory : ModItem
+{
+    public const int TELEPORT_RANGE = 3 * 16;
+    public const int ENEMY_DESIRED_DISTANCE = 3 * 16;
+    public const int ENEMY_SCAN_RANGE = 20 * 16;
+    public const float MAX_ENEMY_ANGLE_FROM_CURSOR = 15 * (MathF.PI / 180f);
+    public override void SetDefaults()
+    {
+        Item.accessory = true;
+        Item.width = 44;
+        Item.height = 64;
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<AmarPlayer>().teleportRange += TELEPORT_RANGE;
+    }
+}
+
+class AmarPlayer : ModPlayer
+{
+    public int teleportRange = 0;
+    public override void ResetEffects()
+    {
+        teleportRange = 0;
+    }
+}
+
+class AmarGlobalItem : GlobalItem
+{
+    public override bool? UseItem(Item item, Player player)
+    {
+        if (player.whoAmI != Main.myPlayer) return null;
+        if (item.DamageType != DamageClass.Melee || player.GetModPlayer<AmarPlayer>().teleportRange <= 0) return null;
+
+        Vector2 lookDirection = Vector2.Normalize(Main.MouseWorld - player.Center);
+
+        float minDotProduct = MathF.Cos(AmarAccessory.MAX_ENEMY_ANGLE_FROM_CURSOR);
+
+        var candidates = Main.npc
+            .Where(npc => npc.active && !npc.friendly)
+            .Select(npc =>
+            {
+                var dist = npc.Hitbox.Distance(player.Center);
+                var diff = npc.Center - player.Center;
+                if (diff == Vector2.Zero)
+                    return (npc, dot: -1f, dist);
+
+                var dir = Vector2.Normalize(diff);
+                var dot = Vector2.Dot(lookDirection, dir);
+                return (npc, dot, dist);
+            })
+            .Where(e =>
+                e.dot >= minDotProduct &&
+                e.dist > AmarAccessory.ENEMY_DESIRED_DISTANCE &&
+                e.dist < AmarAccessory.ENEMY_SCAN_RANGE
+            );
+
+        var best = candidates
+            .OrderByDescending(e => e.dot / e.dist)
+            .FirstOrDefault();
+
+        if (best.npc == null) return null;
+
+        NPC target = best.npc;
+        var tpDistance = Math.Min(player.GetModPlayer<AmarPlayer>().teleportRange,
+            target.Hitbox.Distance(player.Center) - AmarAccessory.ENEMY_DESIRED_DISTANCE);
+        var direction = Vector2.Normalize(target.Center - player.Center);
+        var tpPos = player.position + direction * tpDistance;
+        player.Teleport(tpPos, TeleportationStyleID.RodOfDiscord);
+        return null;
+    }
+}
