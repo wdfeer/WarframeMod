784c74da64c73f412c70bcfda0262c31cefaba83
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-19 18:14:03 +0100

    Add AmarAnguish

---
+++ Content/Items/Accessories/AmarAnguish.cs
@@ -0,0 +1,31 @@
+﻿namespace WarframeMod.Content.Items.Accessories;
+
+public class AmarAnguish : AmarAccessory
+{
+    public override void SetDefaults()
+    {
+        base.SetDefaults();
+        Item.rare = ItemRarityID.Green;
+        Item.value = Item.sellPrice(silver: 33);
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        base.UpdateAccessory(player, hideVisual);
+        player.moveSpeed += 0.05f;
+        player.jumpSpeedBoost += 0.05f;
+    }
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.CrimtaneBar, 2);
+        recipe.AddIngredient(ItemID.Aglet);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
+
+        recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.DemoniteBar, 2);
+        recipe.AddIngredient(ItemID.Aglet);
+        recipe.AddTile(TileID.Anvils);
+        recipe.Register();
+    }
+}
