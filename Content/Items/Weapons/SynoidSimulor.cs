296f8b1ceaffae4912154960af4eb6ead166e503
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-20 18:35:53 +0100

    Add SynoidSimulor

--- Content/Items/Weapons/SynoidSimulor.cs
+++ Content/Items/Weapons/SynoidSimulor.cs
@@ -5,25 +5,26 @@ using WarframeMod.Content.Projectiles;
 
 namespace WarframeMod.Content.Items.Weapons;
 
-public class Simulor : ModItem
+public class SynoidSimulor : ModItem
 {
-    public const int MERGE_DAMAGE_INCREASE_PERCENT = 20;
-    public const int MERGE_DAMAGE_INCREASE_MAX_PERCENT = 300;
+    public const int MERGE_DAMAGE_INCREASE_PERCENT = 40;
+    public const int MERGE_DAMAGE_INCREASE_MAX_PERCENT = 400;
+
     public override void SetDefaults()
     {
-        Item.damage = 42;
-        Item.crit = 8;
+        Item.damage = 77;
+        Item.crit = 10;
         Item.DamageType = DamageClass.Magic;
-        Item.mana = 10;
-        Item.width = 36;
+        Item.mana = 15;
+        Item.width = 31;
         Item.height = 15;
         Item.useTime = 24;
         Item.useAnimation = 24;
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.noMelee = true;
         Item.knockBack = 6.9f;
-        Item.value = Item.sellPrice(gold: 4);
-        Item.rare = ItemRarityID.LightRed;
+        Item.value = Item.sellPrice(gold: 10);
+        Item.rare = ItemRarityID.LightPurple;
         Item.autoReuse = false;
         Item.shoot = ModContent.ProjectileType<SimulorProjectile>();
         Item.shootSpeed = 16f;
@@ -31,6 +32,7 @@ public class Simulor : ModItem
 
     public override bool AltFunctionUse(Player player) => activeProjectileIDs.Count > 0;
     private readonly List<int> activeProjectileIDs = [];
+
     public override void ModifyManaCost(Player player, ref float reduce, ref float mult)
     {
         if (player.altFunctionUse == 2 && activeProjectileIDs.Count > 0)
@@ -38,6 +40,7 @@ public class Simulor : ModItem
             mult = 0;
         }
     }
+
     public override bool Shoot(
         Player player,
         EntitySource_ItemUse_WithAmmo source,
@@ -49,21 +52,14 @@ public class Simulor : ModItem
     {
         if (player.altFunctionUse == 2)
         {
-            float manaMult = 0;
             for (int i = 0; i < activeProjectileIDs.Count; i++)
             {
                 int id = activeProjectileIDs[i];
                 Projectile proj = Main.projectile[id];
 
-                if (proj.active && proj.ModProjectile is SimulorProjectile simulor && simulor.TryExplode())
-                {
-                    manaMult += simulor.DamageMult;
-                }
+                if (proj.active && proj.ModProjectile is SimulorProjectile simulor)
+                    simulor.TryExplode((int)(simulor.ExplosionWidth * simulor.DamageMult));
             }
-
-            int manaGain = (int)(manaMult * 8);
-            player.statMana = Math.Min(player.statMana + manaGain, player.statManaMax2);
-            player.ManaEffect(manaGain);
 
             activeProjectileIDs.Clear();
 
@@ -87,6 +83,7 @@ public class Simulor : ModItem
                 player.whoAmI
             );
             activeProjectileIDs.Add(projID);
+            (Main.projectile[projID].ModProjectile as SimulorProjectile).explosionWidth += 50;
 
             SoundEngine.PlaySound(new SoundStyle("WarframeMod/Content/Sounds/SynoidSimulorSound")
             {
@@ -97,4 +94,3 @@ public class Simulor : ModItem
         }
     }
 }
-
