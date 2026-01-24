fb37f2e8805a8592ed1cf18f8098086a12d95d03
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-24 15:06:57 +0100

    Add BuzzKill

---
+++ Content/Items/Accessories/BuzzKill.cs
@@ -0,0 +1,24 @@
+﻿using Terraria.Localization;
+using WarframeMod.Common;
+using WarframeMod.Common.Players;
+
+namespace WarframeMod.Content.Items.Accessories;
+
+public class BuzzKill : ModItem
+{
+    public const int BLEED_CHANCE_PERCENT = 15;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(BLEED_CHANCE_PERCENT);
+    public override void SetDefaults()
+    {
+        Item.accessory = true;
+        Item.width = 44;
+        Item.height = 64;
+        Item.rare = 4;
+        Item.value = Item.sellPrice(gold: 4);
+    }
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        BuffPlayer buffman = player.GetModPlayer<BuffPlayer>();
+        buffman.AddBleedChance(DamageClass.Melee, BLEED_CHANCE_PERCENT / 100f);
+    }
+}
