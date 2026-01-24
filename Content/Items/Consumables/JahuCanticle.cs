d1ffeb6ac8cc16042d753d69ebd7e7d38b122ea4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-24 17:11:07 +0100

    Fix JahuCanticle not activating

--- Content/Items/Consumables/JahuCanticle.cs
+++ Content/Items/Consumables/JahuCanticle.cs
@@ -1,4 +1,6 @@
+using WarframeMod.Common.GlobalNPCs;
 using WarframeMod.Content.Items.Weapons;
+using WarframeMod.Content.Projectiles;
 
 namespace WarframeMod.Content.Items.Consumables;
 
@@ -12,13 +14,30 @@ public class JahuCanticle : GrimoireUpgrade
         Item.rare = 3;
         Item.value = Item.sellPrice(gold: 2);
     }
+
+    public override void UpdateAccessory(Player player, bool hideVisual)
+    {
+        player.GetModPlayer<JahuCanticlePlayer>().active = true;
+    }
 }
 
 class JahuCanticlePlayer : ModPlayer
 {
+    public bool active;
+    public override void ResetEffects()
+        => active = false;
+
     public override float UseSpeedMultiplier(Item item)
     {
         if (item.ModItem is Grimoire grimoire && grimoire.HasUpgrade(GrimoireUpgradeType.JahuCanticle)) return 1.15f;
         return 1f;
+    }
+
+    public override void OnHitNPCWithProj(Projectile proj, NPC target, NPC.HitInfo hit, int damageDone)
+    {
+        if (active && proj.ModProjectile is GrimoireProjectile or GrimoireAltProjectile)
+        {
+            target.GetGlobalNPC<GrimoireKillGlobalNPC>().Mark(Player, GrimoireUpgradeType.JahuCanticle);
+        }
     }
 }
