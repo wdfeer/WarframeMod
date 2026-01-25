5e00a40af8792112a3193b366e3683e40c88b853
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-25 17:57:12 +0100

    Add UnseenDread

---
+++ Content/Items/Consumables/UnseenDread.cs
@@ -0,0 +1,41 @@
+using Terraria.Localization;
+using WarframeMod.Content.Items.Weapons;
+
+namespace WarframeMod.Content.Items.Consumables;
+
+public class UnseenDread : ModItem
+{
+    public const int CRITICAL_DAMAGE_BONUS_PERCENT = 200;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs($"+{CRITICAL_DAMAGE_BONUS_PERCENT}%");
+    public override void SetDefaults()
+    {
+        Item.width = 44;
+        Item.height = 64;
+        Item.consumable = true;
+        Item.useTime = 30;
+        Item.useAnimation = 30;
+        Item.useStyle = ItemUseStyleID.HoldUp;
+        Item.UseSound = SoundID.Item4;
+    }
+    public override bool? UseItem(Player player)
+    {
+        foreach (Item item in player.inventory)
+        {
+            if (item.ModItem is Dread dread)
+            {
+                dread.unseenDread = true;
+                return true;
+            }
+        }
+        return false;
+    }
+    public override void AddRecipes()
+    {
+        Recipe recipe = CreateRecipe();
+        recipe.AddIngredient(ItemID.HallowedBar, 4);
+        recipe.AddIngredient(ItemID.SoulofNight, 15);
+        recipe.AddIngredient(ItemID.InvisibilityPotion);
+        recipe.AddTile(TileID.MythrilAnvil);
+        recipe.Register();
+    }
+}
--- Content/Items/Weapons/Dread.cs
+++ Content/Items/Weapons/Dread.cs
@@ -1,14 +1,18 @@
 using Terraria.DataStructures;
 using Terraria.GameContent.ItemDropRules;
 using Terraria.Localization;
+using Terraria.ModLoader.IO;
 using WarframeMod.Common.GlobalProjectiles;
+using WarframeMod.Content.Items.Consumables;
 
 namespace WarframeMod.Content.Items.Weapons;
 
 internal class Dread : ModItem
 {
+    public bool unseenDread;
     public const int BLEED_CHANCE = 20;
     public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(BLEED_CHANCE);
+
     public override void SetDefaults()
     {
         Item.damage = 80;
@@ -28,20 +32,48 @@ internal class Dread : ModItem
         Item.shootSpeed = 16f;
         Item.useAmmo = AmmoID.Arrow;
     }
+
+    public override void UpdateInventory(Player player)
+    {
+        Item.rare = unseenDread ? 7 : 5;
+    }
+
     public override Vector2? HoldoutOffset()
     {
         return new Vector2(-1, 0);
     }
-    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+
+    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity,
+        int type, int damage, float knockback)
     {
         WeaponCommon.ModifyProjectileSpawnPosition(ref position, velocity, Item.width);
         int projectileID = Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
         Projectile projectile = Main.projectile[projectileID];
+
         projectile.usesLocalNPCImmunity = true;
         projectile.localNPCHitCooldown = -1;
+
         projectile.GetGlobalProjectile<BuffGlobalProjectile>().AddBleed(BLEED_CHANCE);
+
         (Mod as WarframeMod).SetProjectileExtraUpdatesNetSafe(projectileID, projectile.extraUpdates + 2);
+
+        if (unseenDread && player.HasBuff(BuffID.Invisibility))
+        {
+            projectile.GetGlobalProjectile<CritGlobalProjectile>().CritMultiplier +=
+                UnseenDread.CRITICAL_DAMAGE_BONUS_PERCENT / 100f;
+        }
+
         return false;
+    }
+
+    public override void SaveData(TagCompound tag)
+    {
+        tag["unseenDread"] = unseenDread.ToString();
+    }
+
+    public override void LoadData(TagCompound tag)
+    {
+        unseenDread = bool.Parse(tag.GetString("unseenDread"));
     }
 }
 
@@ -53,8 +85,10 @@ class DreadDropCondition : IItemDropRuleCondition
         int[] playerArmor = info.player.armor.Select(item => item.type).ToArray();
         return necroArmor.All(type => playerArmor.Contains(type));
     }
+
     public bool CanShowItemDropInUI()
         => false;
+
     public string GetConditionDescription()
         => "Necro armor required.";
 }
