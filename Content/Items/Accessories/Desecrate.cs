bae2e631ef0d70ffc724d2cac780a038c044b54b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-26 20:40:48 +0100

    Make Desecrate more stable hopefully

--- Content/Items/Accessories/Desecrate.cs
+++ Content/Items/Accessories/Desecrate.cs
@@ -1,58 +1,72 @@
-﻿namespace WarframeMod.Content.Items.Accessories;
+﻿using Terraria.Localization;
+
+namespace WarframeMod.Content.Items.Accessories;
 
 public class Desecrate : ModItem
 {
     public const int LIFE_DRAIN = 7;
     public const float MAX_DISTANCE = 800;
+
     public override void SetDefaults()
     {
         Item.accessory = true;
         Item.width = 32;
         Item.height = 32;
         Item.rare = 5;
-        Item.value = Item.sellPrice(gold: 9);
+        Item.value = Item.sellPrice(gold: 18);
     }
+
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
-        player.GetModPlayer<DesecratePlayer>().desecrate = true;
+        player.GetModPlayer<DesecratePlayer>().enabled = true;
     }
 }
+
 internal class DesecratePlayer : ModPlayer
 {
-    public bool desecrate = false;
-    public override void ResetEffects() => desecrate = false;
+    public bool enabled;
+    public override void ResetEffects() => enabled = false;
 }
-internal class DesecrateNPC : GlobalNPC
+
+internal class DesecrateGlobalNPC : GlobalNPC
 {
     public override bool InstancePerEntity => true;
-    bool desecrated = false;
-    public override void OnKill(NPC npc)
+    bool desecrated;
+
+    public override bool PreKill(NPC npc)
     {
         if (CanBeDesecrated(npc))
         {
             Player player = NearbyPlayerWithDesecrate(npc);
-            if (player == null)
-                return;
-
-            desecrated = true;
-            npc.NPCLoot();
-            LifeDrainEffect(player);
+            if (player != null)
+            {
+                desecrated = true;
+                npc.NPCLoot();
+                LifeDrainEffect(player);
+            }
         }
+
+        return base.PreKill(npc);
     }
+
     public bool CanBeDesecrated(NPC npc)
         => !desecrated && !npc.boss && npc.CanBeChasedBy();
+
     private Player NearbyPlayerWithDesecrate(NPC npc)
     {
         return Array.Find(Main.player, player =>
-                                    player.active
-                                    && player.GetModPlayer<DesecratePlayer>().desecrate
-                                    && player.position.Distance(npc.position) < Desecrate.MAX_DISTANCE
+            player.active
+            && player.GetModPlayer<DesecratePlayer>().enabled
+            && player.position.Distance(npc.position) < Desecrate.MAX_DISTANCE
         );
     }
+
     private void LifeDrainEffect(Player player)
     {
-        Terraria.DataStructures.PlayerDeathReason reason = new() { SourceCustomReason = player.name + " was desecrated" };
-        player.Hurt(reason, Desecrate.LIFE_DRAIN, 0, scalingArmorPenetration: 1f, cooldownCounter: -2);
+        Terraria.DataStructures.PlayerDeathReason reason = new()
+            { CustomReason = NetworkText.From(player.name + " was desecrated") };
+        player.Hurt(reason, Desecrate.LIFE_DRAIN, 0, dodgeable: false, scalingArmorPenetration: 1f,
+            cooldownCounter: -2);
         player.netLife = true;
     }
 }
