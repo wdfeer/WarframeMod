8dfd6d5d61ceed65c6c4ca0c50e8600e83a45e24
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-26 14:58:05 +0100

    Refactor summon bleeding localization

--- Content/Items/Accessories/HunterAccessory.cs
+++ Content/Items/Accessories/HunterAccessory.cs
@@ -1,10 +1,13 @@
-﻿using WarframeMod.Common.Players;
+﻿using Terraria.Localization;
+using WarframeMod.Common.Players;
 
 namespace WarframeMod.Content.Items.Accessories;
 
 public abstract class HunterAccessory : ModItem
 {
     public const float MINION_BLEED_CHANCE_PERCENT = 7.5f;
+    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(MINION_BLEED_CHANCE_PERCENT.ToString("F1"));
+
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
         player.GetModPlayer<BuffPlayer>().AddBleedChance(DamageClass.Summon, MINION_BLEED_CHANCE_PERCENT / 100f);
