cbbcefe383127e6288dff5b46a22ea3e4006e4bf
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-27 18:36:41 +0100

    Fix Reach not working with CalamityMod

--- Common/GlobalItems/TrueMeleeRangeGlobalItem.cs
+++ Common/GlobalItems/TrueMeleeRangeGlobalItem.cs
@@ -6,7 +6,7 @@ class TrueMeleeRangeGlobalItem : GlobalItem
 {
     public override void UseItemHitbox(Item item, Player player, ref Rectangle hitbox, ref bool noHitbox)
     {
-        if (item.noMelee || item.DamageType != DamageClass.Melee && item.DamageType != DamageClass.MeleeNoSpeed)
+        if (item.noMelee)
             return;
         var modPl = player.GetModPlayer<TrueMeleeRangePlayer>();
         if (modPl.rangeMult == 1f && modPl.absoluteExtraRange == 0)
