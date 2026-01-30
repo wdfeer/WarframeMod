5564c2c3f6f48466ebe2a828f26da4b2580618ab
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-01-30 20:00:47 +0100

    Add CrewmanHelmet glowmask

--- Content/Items/Armor/CrewmanHelmet.cs
+++ Content/Items/Armor/CrewmanHelmet.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
 using Terraria.Localization;
 
 namespace WarframeMod.Content.Items.Armor;
@@ -7,6 +9,13 @@ public class CrewmanHelmet : ModItem
 {
     public const int CRIT_CHANCE = 3;
     public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(CRIT_CHANCE);
+
+    private static Asset<Texture2D> glowTexture;
+
+    public override void Load()
+    {
+        glowTexture = ModContent.Request<Texture2D>(Texture + "_Glow");
+    }
 
     public override void SetStaticDefaults()
     {
@@ -34,5 +43,29 @@ public class CrewmanHelmet : ModItem
     public override void UpdateEquip(Player player)
     {
         player.GetCritChance<GenericDamageClass>() += 3;
+    }
+
+    // TODO: add glowmask for when actually wearing the helmet
+    public override void PostDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, float rotation,
+        float scale,
+        int whoAmI)
+    {
+        Texture2D texture = glowTexture.Value;
+        spriteBatch.Draw
+        (
+            texture,
+            new Vector2
+            (
+                Item.position.X - Main.screenPosition.X + Item.width * 0.5f,
+                Item.position.Y - Main.screenPosition.Y + Item.height - texture.Height * 0.5f
+            ),
+            new Rectangle(0, 0, texture.Width, texture.Height),
+            Color.White,
+            rotation,
+            texture.Size() * 0.5f,
+            scale,
+            SpriteEffects.None,
+            0f
+        );
     }
 }
