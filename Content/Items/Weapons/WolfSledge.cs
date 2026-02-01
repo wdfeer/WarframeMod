505edffb93652efdb6972263d58300fd9c5a3f9f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-01 09:47:04 +0100

    Add WolfSledge

---
+++ Content/Items/Weapons/WolfSledge.cs
@@ -0,0 +1,42 @@
+using Terraria.Localization;
+using WarframeMod.Common.GlobalItems;
+using WarframeMod.Content.Projectiles;
+
+
+namespace WarframeMod.Content.Items.Weapons;
+
+internal class WolfSledge : CircularMelee
+{
+    public override void SetDefaults()
+    {
+        Item.damage = 240;
+        Item.crit = 13;
+        Item.knockBack = 12f;
+        Item.DamageType = DamageClass.Melee;
+        Item.width = 56;
+        Item.height = 56;
+        Item.scale = 2.5f;
+        Item.useStyle = ItemUseStyleID.Swing;
+        Item.UseSound = SoundID.Item1;
+        Item.useTime = 60;
+        Item.useAnimation = 60;
+        Item.autoReuse = true;
+        Item.rare = ItemRarityID.Yellow;
+        Item.value = Item.sellPrice(gold: 16);
+    }
+
+    public override void AddRecipes()
+        => CreateRecipe()
+            .AddIngredient(ItemID.TurtleShell, 8)
+            .AddIngredient(ItemID.SpookyWood, 30)
+            .AddCondition(Condition.DownedPlantera)
+            .AddTile(TileID.MythrilAnvil)
+            .Register();
+
+    public override float SizeMult => 2f;
+    
+    public override float UseSpeedMultiplier(Player player)
+    {
+        return player.GetAttackSpeed(Item.DamageType);
+    }
+}
