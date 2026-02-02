af0c8a87d39a447543e13af3778383849eb4686f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-02 14:58:46 +0100

    Update dropped arcane drawing

--- Content/Items/Arcanes/Arcane.cs
+++ Content/Items/Arcanes/Arcane.cs
@@ -1,4 +1,6 @@
-﻿namespace WarframeMod.Content.Items.Arcanes;
+﻿using Microsoft.Xna.Framework.Graphics;
+
+namespace WarframeMod.Content.Items.Arcanes;
 
 public abstract class Arcane : ModItem
 {
@@ -40,7 +42,7 @@ public abstract class Arcane : ModItem
     public override void SetDefaults()
     {
         Item.accessory = true;
-        Item.rare = -12;
+        Item.rare = ItemRarityID.Expert;
         Item.expert = true;
         Item.value = Item.sellPrice(gold: 4);
     }
@@ -53,5 +55,21 @@ public abstract class Arcane : ModItem
     public override bool CanAccessoryBeEquippedWith(Item equippedItem, Item incomingItem, Player player)
     {
         return equippedItem.ModItem is not Arcane || incomingItem.ModItem is not Arcane;
+    }
+
+    public override bool PreDrawInWorld(SpriteBatch spriteBatch,
+        Color lightColor,
+        Color alphaColor,
+        ref float rotation,
+        ref float scale,
+        int whoAmI)
+    {
+        Texture2D texture = ModContent.Request<Texture2D>("WarframeMod/Assets/DroppedArcane").Value;
+        Vector2 pos = Item.Center - new Vector2(texture.Width / 2f, texture.Height / 2f) - Main.screenPosition;
+        Rectangle drawRect = new((int)pos.X, (int)pos.Y, texture.Width, texture.Height);
+        Vector2 origin = new Vector2(texture.Width / 2f, texture.Height / 2f);
+        spriteBatch.Draw(texture, drawRect, null, lightColor, rotation, origin, SpriteEffects.None, 0f);
+
+        return false;
     }
 }
