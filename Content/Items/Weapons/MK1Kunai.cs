4b096d5e5366b0d32548c4021f6f2312421a893b
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-08 16:46:29 +0100

    Fix Mk1Kunai having recipes

--- Content/Items/Weapons/MK1Kunai.cs
+++ Content/Items/Weapons/MK1Kunai.cs
@@ -3,6 +3,7 @@ namespace WarframeMod.Content.Items.Weapons;
 public class MK1Kunai : Kunai
 {
     public override string Texture => "WarframeMod/Content/Items/Weapons/Kunai";
+
     public override void SetDefaults()
     {
         base.SetDefaults();
@@ -11,5 +12,9 @@ public class MK1Kunai : Kunai
         Item.rare = 1;
         Item.value = Item.buyPrice(silver: 50);
         Item.shootSpeed = 19f;
+    }
+
+    public override void AddRecipes()
+    {
     }
 }
