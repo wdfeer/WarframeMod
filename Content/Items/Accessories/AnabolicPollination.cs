8846ea38c2768b69b19105c6433e87bddfb3b4f1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-12 17:12:21 +0100

    Rework AnabolicPollination

--- Content/Items/Accessories/AnabolicPollination.cs
+++ Content/Items/Accessories/AnabolicPollination.cs
@@ -1,15 +1,15 @@
 using Terraria.Localization;
-using WarframeMod.Common.GlobalNPCs;
+using WarframeMod.Common;
+using WarframeMod.Common.Players;
 
 namespace WarframeMod.Content.Items.Accessories;
+
 public class AnabolicPollination : ModItem
 {
-    public const int POISON_CHANCE_PER_MINION_PERCENT = 3;
-    public const int EXTRA_POISON_DPS_PER_MINION = 35;
-    public const int POISON_DURATION = 300;
+    public const int TOXIN_CHANCE_PER_MINION_SLOT = 3;
 
     public override LocalizedText Tooltip =>
-        base.Tooltip.WithFormatArgs(POISON_CHANCE_PER_MINION_PERCENT, EXTRA_POISON_DPS_PER_MINION);
+        base.Tooltip.WithFormatArgs(TOXIN_CHANCE_PER_MINION_SLOT);
 
     public override void SetDefaults()
     {
@@ -17,39 +17,10 @@ public class AnabolicPollination : ModItem
         Item.rare = ItemRarityID.Lime;
         Item.value = Item.buyPrice(gold: 30);
     }
+
     public override void UpdateAccessory(Player player, bool hideVisual)
     {
-        player.GetModPlayer<AnabolicPollinationPlayer>().enabled = true;
-    }
-}
-
-class AnabolicPollinationPlayer : ModPlayer
-{
-    public bool enabled;
-    public override void ResetEffects()
-        => enabled = false;
-
-    private float PoisonChance => (Player.maxMinions - 1) * AnabolicPollination.POISON_CHANCE_PER_MINION_PERCENT / 100f;
-    private float PoisonDPS => (Player.maxMinions - 1) * AnabolicPollination.EXTRA_POISON_DPS_PER_MINION;
-    void OnHitNPCWithSomething(NPC target)
-    {
-        if (!enabled || PoisonChance <= 0f)
-            return;
-        if (Main.rand.NextFloat() < PoisonChance)
-        {
-            target.AddBuff(BuffID.Poisoned, AnabolicPollination.POISON_DURATION);
-            target.GetGlobalNPC<DebuffDamageGlobalNPC>()
-                .AddBuffDamage(DebuffDamageGlobalNPC.SourceId.AnabolicPollination,
-                    BuffID.Poisoned,
-                    (int)PoisonDPS);
-        }
-    }
-    public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
-    {
-        OnHitNPCWithSomething(target);
-    }
-    public override void OnHitNPCWithProj(Projectile proj, NPC target, NPC.HitInfo hit, int damageDone)
-    {
-        OnHitNPCWithSomething(target);
+        var mult = player.maxMinions;
+        player.GetModPlayer<BuffPlayer>().AddBuffChance(StackableBuff.Toxin, TOXIN_CHANCE_PER_MINION_SLOT * mult);
     }
 }
