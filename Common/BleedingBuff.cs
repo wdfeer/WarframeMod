bdf6e7a8bf4b11f7da8e9971bc7c7f3e77592fd1
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-12 16:59:47 +0100

    Fix making electricity instead of bleeds

--- Common/BleedingBuff.cs
+++ Common/BleedingBuff.cs
@@ -3,5 +3,5 @@ public class BleedingBuff
 {
     [Obsolete]
     public static void Create(float hitDamage, NPC target, bool netSync = true)
-        => DotBuff.Create(StackableBuff.Electricity, (int)hitDamage, target, netSync);
+        => DotBuff.Create(StackableBuff.Bleeding, (int)hitDamage, target, netSync);
 }
