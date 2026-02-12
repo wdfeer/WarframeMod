fce4487b1187dd64e7d72c3a2a464e2a048afa1a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-12 09:44:37 +0100

    Refactor BuffPlayer

--- Common/Players/BuffPlayer.cs
+++ Common/Players/BuffPlayer.cs
@@ -1,59 +1,52 @@
 ﻿namespace WarframeMod.Common.Players;
+
 public class BuffPlayer : CritPlayerHooks
 {
     public List<BuffChance> buffsOnHitNPC;
-    public List<StackableBuffChance> stackableBuffsOnHitNPC;
-    Dictionary<int, float> bleedingChances = new Dictionary<int, float>();
-    int GetDamageClassID(DamageClass damageClass)
+
+    public Dictionary<DamageClass, List<StackableBuffChance>> stackableBuffsOnHitNPC
+        = new() { { DamageClass.Generic, new() } };
+
+    public void AddBuffChance(StackableBuff type, int chancePercent, DamageClass damageClass = null)
     {
-        if (damageClass == DamageClass.SummonMeleeSpeed)
-            return DamageClass.Summon.Type;
-        if (damageClass == DamageClass.MeleeNoSpeed)
-            return DamageClass.Melee.Type;
-        return damageClass.Type;
+        if (damageClass == null)
+            damageClass = DamageClass.Generic;
+
+        stackableBuffsOnHitNPC[damageClass].Add(new StackableBuffChance(type, chancePercent));
     }
+
+    [Obsolete]
     public void AddBleedChance(DamageClass damageType, float chance)
     {
-        int type = GetDamageClassID(damageType);
-        if (bleedingChances.ContainsKey(type))
-        {
-            bleedingChances[type] += chance;
-        }
-        else
-        {
-            bleedingChances.Add(type, chance);
-        }
+        AddBuffChance(StackableBuff.Bleeding, (int)(chance * 100f), damageType);
     }
-    public void ApplyBleedChances(NPC target, int damage, DamageClass damageType)
-    {
-        int type = GetDamageClassID(damageType);
-        if (!bleedingChances.ContainsKey(type))
-            return;
-        if (Main.rand.NextFloat() < bleedingChances[type])
-            BleedingBuff.Create(damage, target);
-    }
+
     public override void ResetEffects()
     {
         buffsOnHitNPC = [];
-        stackableBuffsOnHitNPC = [];
-        bleedingChances = new();
+        stackableBuffsOnHitNPC = new()
+            { { DamageClass.Generic, new() } };
     }
+
     public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
-    {
-        BuffChance.ApplyBuffs(target, buffsOnHitNPC);
-    }
+        => BuffChance.ApplyBuffs(target, buffsOnHitNPC);
+
     public override void ModifyHitNPCWithProj(Projectile proj, NPC target, ref NPC.HitModifiers modifiers)
+        => BuffChance.ApplyBuffs(target, buffsOnHitNPC);
+
+    public override void OnHitNPCPostCrit(Item item, NPC target, int damage, float knockback, bool crit, float critMult,
+        int critLvl, int damagePostCrit)
     {
-        BuffChance.ApplyBuffs(target, buffsOnHitNPC);
+        foreach (var pair in stackableBuffsOnHitNPC)
+            if (item.DamageType.CountsAsClass(pair.Key))
+                StackableBuffChance.ApplyBuffs(target, pair.Value, damagePostCrit);
     }
-    public override void OnHitNPCPostCrit(Item item, NPC target, int damage, float knockback, bool crit, float critMult, int critLvl, int damagePostCrit)
+
+    public override void OnHitNPCWithProjPostCrit(Projectile proj, NPC target, int damage, float knockback, bool crit,
+        float critMult, int critLvl, int damagePostCrit)
     {
-        ApplyBleedChances(target, damagePostCrit, item.DamageType);
-        StackableBuffChance.ApplyBuffs(target, stackableBuffsOnHitNPC, damagePostCrit);
-    }
-    public override void OnHitNPCWithProjPostCrit(Projectile proj, NPC target, int damage, float knockback, bool crit, float critMult, int critLvl, int damagePostCrit)
-    {
-        ApplyBleedChances(target, damagePostCrit, proj.DamageType);
-        StackableBuffChance.ApplyBuffs(target, stackableBuffsOnHitNPC, damagePostCrit);
+        foreach (var pair in stackableBuffsOnHitNPC)
+            if (proj.DamageType.CountsAsClass(pair.Key))
+                StackableBuffChance.ApplyBuffs(target, pair.Value, damagePostCrit);
     }
 }
