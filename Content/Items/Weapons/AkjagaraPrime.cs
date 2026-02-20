059f2606d73e5a49b5884e736dc081458ea27b3f
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-20 18:06:18 +0100

    Add penetration to AkjagaraPrime

--- Content/Items/Weapons/AkjagaraPrime.cs
+++ Content/Items/Weapons/AkjagaraPrime.cs
@@ -10,7 +10,10 @@ public class AkjagaraPrime : ModItem
     private const int BASE_BLEED_CHANCE = 30;
     private int bleedChance = BASE_BLEED_CHANCE;
     private const int CRIT_DAMAGE_INCREASE = 10;
-    public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(BASE_BLEED_CHANCE, $"+{CRIT_DAMAGE_INCREASE}%");
+
+    public override LocalizedText Tooltip =>
+        base.Tooltip.WithFormatArgs(BASE_BLEED_CHANCE, $"+{CRIT_DAMAGE_INCREASE}%");
+
     public override void ModifyTooltips(List<TooltipLine> tooltips)
     {
         UpdateBleedChance(Main.LocalPlayer);
@@ -39,11 +42,13 @@ public class AkjagaraPrime : ModItem
         Item.shoot = 10;
         Item.shootSpeed = 16f;
         Item.useAmmo = AmmoID.Bullet;
-        Item.UseSound = new SoundStyle("WarframeMod/Content/Sounds/AkjagaraPrimeSound").ModifySoundStyle(pitchVariance: 0.1f);
+        Item.UseSound =
+            new SoundStyle("WarframeMod/Content/Sounds/AkjagaraPrimeSound").ModifySoundStyle(pitchVariance: 0.1f);
     }
 
     public override void AddRecipes()
-        => CreateRecipe().AddIngredient(ItemID.HallowedBar, 12).AddIngredient(ItemID.DungeonDesertKey).AddTile(TileID.MythrilAnvil)
+        => CreateRecipe().AddIngredient(ItemID.HallowedBar, 12).AddIngredient(ItemID.DungeonDesertKey)
+            .AddTile(TileID.MythrilAnvil)
             .Register();
 
 
@@ -51,20 +56,23 @@ public class AkjagaraPrime : ModItem
         int type, int damage, float knockback)
     {
         SoundEngine.PlaySound(SoundID.Item11.WithVolumeScale(0.9f), position);
-        
+
         UpdateBleedChance(player);
 
         for (int i = 0; i < 2; i++)
         {
             if (i == 1) velocity *= 0.95f;
-            
+
             var proj = this.ShootWith(player, source, position, velocity, type, damage, knockback, 0.004f, Item.width);
             proj.GetGlobalProjectile<BuffGlobalProjectile>().AddBleed(bleedChance);
             proj.GetGlobalProjectile<CritGlobalProjectile>().CritMultiplier += CRIT_DAMAGE_INCREASE / 100f;
             proj.usesLocalNPCImmunity = true;
             proj.localNPCHitCooldown = -1;
+            
+            if (proj.penetrate == 0)
+                proj.penetrate = 1;
         }
-        
+
         return false;
     }
 
