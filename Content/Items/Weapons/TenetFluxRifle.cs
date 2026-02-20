a6452a550bf918e4ecd6c146e86447d809810d03
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-20 19:41:09 +0100

    Add TenetFluxRifle

--- Content/Items/Weapons/TenetFluxRifle.cs
+++ Content/Items/Weapons/TenetFluxRifle.cs
@@ -3,31 +3,31 @@ using Terraria.Localization;
 
 namespace WarframeMod.Content.Items.Weapons;
 
-public class FluxRifle : ModItem
+public class TenetFluxRifle : ModItem
 {
     public const int BLEED_CHANCE = 100;
     public override LocalizedText Tooltip => base.Tooltip.WithFormatArgs(BLEED_CHANCE);
 
     public override void SetDefaults()
     {
-        Item.damage = 14;
-        Item.crit = 6;
+        Item.damage = 40;
+        Item.crit = 16;
         Item.DamageType = DamageClass.Magic;
-        Item.mana = 9;
+        Item.mana = 12;
         Item.width = 45;
-        Item.height = 16;
+        Item.height = 15;
         Item.useTime = 5;
         Item.useAnimation = 5;
         Item.useStyle = ItemUseStyleID.Shoot;
         Item.noMelee = true;
-        Item.knockBack = 0;
-        Item.value = Item.sellPrice(gold: 2);
-        Item.rare = ItemRarityID.Orange;
+        Item.knockBack = 0.2f;
+        Item.value = Item.sellPrice(gold: 4);
+        Item.rare = 9;
         Item.autoReuse = true;
         Item.shootSpeed = 8f;
         Item.shoot = ModContent.ProjectileType<Projectiles.FluxRifleProjectile>();
         Item.UseSound = new Terraria.Audio.SoundStyle("WarframeMod/Content/Sounds/TenetFluxRifleSound")
-            .ModifySoundStyle(volume: 0.75f, pitchVariance: 0.1f);
+            .ModifySoundStyle(volume: 0.75f, pitchVariance: 0.12f);
     }
 
     public override Vector2? HoldoutOffset()
@@ -37,18 +37,11 @@ public class FluxRifle : ModItem
 
     public override void AddRecipes()
     {
-        var platinum = new RecipeGroup(() => "Any Platinum Bar", ItemID.PlatinumBar, ItemID.GoldBar);
         Recipe recipe = CreateRecipe();
-        recipe.AddRecipeGroup(platinum, 9);
-        recipe.AddIngredient(ItemID.TissueSample, 9);
-        recipe.AddTile(TileID.Anvils);
-        recipe.Register();
-
-        recipe = CreateRecipe();
-        recipe.AddRecipeGroup(platinum, 9);
-        recipe.AddIngredient(ItemID.ShadowScale, 9);
-        recipe.AddTile(TileID.Anvils);
-        recipe.Register();
+        recipe.AddIngredient<FluxRifle>();
+        recipe.AddIngredient<Fieldron>();
+        recipe.AddTile(TileID.MythrilAnvil);
+        recipe.Register(); 
     }
 
     public override void ModifyShootStats(Player player, ref Vector2 position, ref Vector2 velocity, ref int type,
