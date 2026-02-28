c8ec5727e39796047c5a66ef0d6e47dbdf317709
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-02-28 09:51:40 +0100

    Unhardcode ArcaneGuardianBuff tooltip

--- Content/Buffs/ArcaneGuardianBuff.cs
+++ Content/Buffs/ArcaneGuardianBuff.cs
@@ -1,3 +1,4 @@
+using Humanizer;
 using WarframeMod.Content.Items.Arcanes;
 
 namespace WarframeMod.Content.Buffs;
@@ -11,7 +12,7 @@ public class ArcaneGuardianBuff : ModBuff
     int defense = 1;
     public override void ModifyBuffText(ref string buffName, ref string tip, ref int rare)
     {
-        tip = $"+{defense} Defense";
+        tip = tip.FormatWith(defense);
     }
     public override void Update(Player player, ref int buffIndex)
     {
