2aee5761429c34f2d93996e7a046f1cbb710e4e4
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-04-09 11:21:07 +0200

    Create EximusType enum

--- Common/GlobalNPCs/EximusGlobalNPC.cs
+++ Common/GlobalNPCs/EximusGlobalNPC.cs
@@ -4,11 +4,23 @@ namespace WarframeMod.Common.GlobalNPCs;
 
 public class EximusGlobalNPC : GlobalNPC
 {
+    public enum EximusType
+    {
+        None = 0,
+        Shock,
+        // TODO: implement more eximus types
+    }
+    public override bool InstancePerEntity => true;
+    public EximusType eximus;
     public override void SetDefaults(NPC entity)
     {
         base.SetDefaults(entity);
-        if (!entity.boss && Main.rand.Next(100) < ModContent.GetInstance<WarframeServerConfig>().eximusChancePercent) {
-            // TODO: make `entity` an eximus unit
+        if (!entity.boss && Main.rand.Next(100) < ModContent.GetInstance<WarframeServerConfig>().eximusChancePercent)
+        {
+            var allTypes = Enum.GetValues<EximusType>();
+            eximus = allTypes[Main.rand.Next(allTypes.Length - 1) + 1]; // without None
+            
+            // TODO: increase enemy hp
         }
     }
 }
